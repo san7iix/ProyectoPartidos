10949caddc0545877f97976b8195777141a38d2a
Santiago Martinez <xsantysxx@gmail.com>
2020-10-08 17:21:51 -0500

    Creado index de la web

---
+++ client/src/componentes/Inicio.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inicio extends Component {
+    render() {
+        return (
+            <div>
+                Inicio
+            </div>
+        );
+    }
+}
+
+export default Inicio;
---
+++ client/src/componentes/Usuarios.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inicio extends Component {
+    render() {
+        return (
+            <div>
+                Usuarios
+            </div>
+        );
+    }
+}
+
+export default Inicio;
