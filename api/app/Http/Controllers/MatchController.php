ccec7735a8634ccf330349590a2b4f17d8fee770
jagueto98 <iblackhollow98@gmail.com>
2020-10-09 12:38:01 -0500

    Modify controllers

--- api/app/Http/Controllers/MatchController.php
+++ api/app/Http/Controllers/MatchController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class MatchController extends Controller
 {
     /**
      * Display a listing of the resource.
