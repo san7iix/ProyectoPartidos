6973f58b5a9aca9f8fc4c6e3b90e84efa4e5598a
jagueto98 <iblackhollow98@gmail.com>
2020-10-15 05:42:52 -0500

    Update footred_db.sql

--- api/footred_db.sql
+++ api/footred_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2020 a las 08:59:46
+-- Tiempo de generación: 15-10-2020 a las 12:42:17
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
@@ -57,7 +57,7 @@ CREATE TABLE `managers` (
 
 INSERT INTO `managers` (`id`, `id_team`, `id_user`, `created_at`, `updated_at`) VALUES
 (1, 1, 2, '2020-10-15 10:20:49', '2020-10-15 10:20:49'),
-(2, NULL, 6, '2020-10-15 11:00:54', '2020-10-15 11:00:54');
+(2, 29, 6, '2020-10-15 11:00:54', '2020-10-15 12:26:46');
 
 -- --------------------------------------------------------
 
@@ -73,15 +73,16 @@ CREATE TABLE `matches` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `state` int(10) UNSIGNED DEFAULT NULL,
-  `date` date DEFAULT NULL
+  `date` date DEFAULT NULL,
+  `hour` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `matches`
 --
 
-INSERT INTO `matches` (`id`, `id_team_1`, `id_team_2`, `id_place`, `created_at`, `updated_at`, `state`, `date`) VALUES
-(3, 1, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `matches` (`id`, `id_team_1`, `id_team_2`, `id_place`, `created_at`, `updated_at`, `state`, `date`, `hour`) VALUES
+(6, 1, 29, 1, '2020-10-15 14:59:01', '2020-10-15 14:59:01', 1, '2020-10-15', NULL);
 
 -- --------------------------------------------------------
 
@@ -117,7 +118,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (99, '2020_10_15_050845_create_relationships_manager_teams', 4),
 (100, '2020_10_15_051338_create_relation', 5),
 (101, '2020_10_15_051704_create_relation', 6),
-(102, '2020_10_15_052808_create_relation', 7);
+(102, '2020_10_15_052808_create_relation', 7),
+(103, '2020_10_15_101044_create_hour_column_from_match', 8);
 
 -- --------------------------------------------------------
 
@@ -145,6 +147,13 @@ CREATE TABLE `places` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `places`
+--
+
+INSERT INTO `places` (`id`, `name`, `price`, `created_at`, `updated_at`) VALUES
+(1, 'MONDA', '5', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -166,8 +175,8 @@ CREATE TABLE `players` (
 
 INSERT INTO `players` (`id`, `id_user`, `id_team`, `id_manager`, `created_at`, `updated_at`) VALUES
 (1, 3, 2, NULL, '2020-10-15 10:35:39', '2020-10-15 10:35:39'),
-(2, 4, NULL, NULL, '2020-10-15 10:36:29', '2020-10-15 10:36:29'),
-(3, 5, NULL, NULL, '2020-10-15 10:36:34', '2020-10-15 10:36:34');
+(2, 4, NULL, NULL, '2020-10-15 10:36:29', '2020-10-15 15:26:38'),
+(3, 5, 29, NULL, '2020-10-15 10:36:34', '2020-10-15 10:36:34');
 
 -- --------------------------------------------------------
 
@@ -212,7 +221,8 @@ CREATE TABLE `teams` (
 
 INSERT INTO `teams` (`id`, `name`, `uniform`, `created_at`, `updated_at`, `id_manager`) VALUES
 (1, 'TeaamPro', 'Manchester', NULL, NULL, 2),
-(2, 'LOS 3HP', 'ADSAD', NULL, NULL, NULL);
+(2, 'LOS 3HP', 'ADSAD', NULL, NULL, NULL),
+(29, 'LOS 2 HP', 'azul fosforescente con amarillo', '2020-10-15 12:26:46', '2020-10-15 12:26:46', 6);
 
 -- --------------------------------------------------------
 
@@ -238,11 +248,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `id_role`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'javier', 'javier@mail.com', NULL, '$2y$10$5pp9tY9mASMp.rEoTA6u6.JQRIMmDiQTxXaH.sK2N8xGge1oF2Z3O', 1, NULL, '2020-10-10 00:25:50', '2020-10-10 00:25:50'),
-(2, 'javeir', 'jadasa@mail.com', NULL, '$2y$10$Vs9vRK/YhWNIpbXZ2zmTYecwYPelqAnOepwEOJBKgW4wm0RU.FVA2', 3, NULL, '2020-10-15 10:20:49', '2020-10-15 10:20:49'),
+(2, 'monda2', 'jadasa@mail.com', NULL, '$2y$10$Vs9vRK/YhWNIpbXZ2zmTYecwYPelqAnOepwEOJBKgW4wm0RU.FVA2', 3, NULL, '2020-10-15 10:20:49', '2020-10-15 10:20:49'),
 (3, 'javeir', 'jadaaasa@mail.com', NULL, '$2y$10$zHU9LSW6xZMuMV6q7ACOHe/wN5R3MqfAUWIWLCtVBGTTQTPYsH5se', 2, NULL, '2020-10-15 10:35:39', '2020-10-15 10:35:39'),
 (4, 'javeir', 'jadaa3asa@mail.com', NULL, '$2y$10$cvVZwidq2h36Czdnj.1V/eZmlgsmnSqaiIEjTHfbiY5AZiOSPEPuW', 2, NULL, '2020-10-15 10:36:29', '2020-10-15 10:36:29'),
 (5, 'javeir', 'jad11aa3asa@mail.com', NULL, '$2y$10$Bej8PaQ79/eidbUFbc43yO7zkeIMa8QQxpmIA0SQ7DU35oUqTRgNy', 2, NULL, '2020-10-15 10:36:34', '2020-10-15 10:36:34'),
-(6, 'javeir', 'jad111aa3asa@mail.com', NULL, '$2y$10$aqezXNOuK5TD4wRItgj96u9Q3gYx4PNG8ibyvbsl21y6k8whQUDCa', 3, NULL, '2020-10-15 11:00:54', '2020-10-15 11:00:54');
+(6, 'monda', 'jad111aa3asa@mail.com', NULL, '$2y$10$aqezXNOuK5TD4wRItgj96u9Q3gYx4PNG8ibyvbsl21y6k8whQUDCa', 3, NULL, '2020-10-15 11:00:54', '2020-10-15 11:00:54');
 
 --
 -- Índices para tablas volcadas
@@ -340,19 +350,19 @@ ALTER TABLE `managers`
 -- AUTO_INCREMENT de la tabla `matches`
 --
 ALTER TABLE `matches`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT de la tabla `places`
 --
 ALTER TABLE `places`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `players`
@@ -370,7 +380,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
